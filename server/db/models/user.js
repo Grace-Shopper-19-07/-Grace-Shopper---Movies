bb931f9e9f404b2740cf6bdde2660cc12b166afb
Jonathan Galletta <jonathan.galletta@gmail.com>
2019-09-17 14:52:03 -0400

    updated user model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -21,14 +21,6 @@ const User = db.define('user', {
       return () => this.getDataValue('password')
     }
   },
-  cart: {
-    type: Sequelize.ARRAY,
-    defaultValue: []
-  },
-  history: {
-    type: Sequelize.ARRAY,
-    defaultValue: []
-  },
   salt: {
     type: Sequelize.STRING,
     // Making `.salt` act like a function hides it when serializing to JSON.
