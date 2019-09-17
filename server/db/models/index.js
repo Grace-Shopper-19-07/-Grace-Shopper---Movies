d719b598577c0155ea631aa1799a4460779c7686
Jonathan Galletta <jonathan.galletta@gmail.com>
2019-09-17 17:11:31 -0400

    fixed user to order association

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -7,7 +7,7 @@ const ProductOrder = require('./productorder')
 Address.belongsToMany(User)
 User.belongsToMany(Address)
 
-User.hasOne(Order)
+User.hasMany(Order)
 Order.hasMany(User)
 
 Movie.belongsToMany(Order, {through: 'ProductOrder'})
