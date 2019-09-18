6be62ad8d97524bca8264ed54cdac04e0e006114
Jonathan Galletta <jonathan.galletta@gmail.com>
2019-09-18 15:19:26 -0400

    made code review changes minus a couple (noted in github project page), updated seed file

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -10,6 +10,9 @@ const User = db.define('user', {
   },
   password: {
     type: Sequelize.STRING,
+    validate: {
+      isEmail: true
+    },
     // Making `.password` act like a func hides it when serializing to JSON.
     // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
