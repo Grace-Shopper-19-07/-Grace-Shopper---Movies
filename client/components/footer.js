ee3a7c844b999426af76d9446cf841baa291532d
Michael Cheung <mcheung125@gmail.com>
2019-09-20 16:14:18 -0400

    cart refresh bug fixed

--- client/components/footer.js
+++ client/components/footer.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {getUserCartById} from '../store/cartReducer'
+import {connect} from 'react-redux'
 
-const Footer = () => (
-  <div className="footer">
-    <Link to="./contact">Contact Us</Link>
-    <Link to="./about">About Us</Link>
-  </div>
-)
+// const Footer = () => (
+//   <div className="footer">
+//     <Link to="./contact">Contact Us</Link>
+//     <Link to="./about">About Us</Link>
+//   </div>
+// )
 
-export default Footer
+class Footer extends React.Component {
+  componentDidMount() {
+    this.props.renderCart()
+  }
+  render() {
+    return (
+      <div className="footer">
+        <Link to="./contact">Contact Us</Link>
+        <Link to="./about">About Us</Link>
+      </div>
+    )
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    renderCart: id => {
+      dispatch(getUserCartById())
+    }
+  }
+}
+
+export default connect(null, mapDispatch)(Footer)
