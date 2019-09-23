39e4004def3268636b4d0d4945219b02cb2b3549
Jonathan Galletta <jonathan.galletta@gmail.com>
2019-09-23 17:42:46 -0400

    added checkout page, postcheckout page routing

--- client/components/checkout.js
+++ client/components/checkout.js
@@ -1,10 +1,79 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {logout} from '../store'
+import {
+  getUserCartById,
+  checkOutThunk,
+  removeMovieThunk
+} from '../store/cartReducer'
 
-export default () => (
-  <div>
-    <p>
-      Thank you for your purchase. Please check your email for details regarding
-      shipping, handling and tracking.
-    </p>
-  </div>
-)
+class Checkout extends React.Component {
+  componentDidMount() {
+    if (this.props.user.id) this.props.renderCart()
+  }
+
+  render() {
+    let totalPrice = 0
+    this.props.cart.movies.map(movie => (totalPrice += movie.price))
+    if (this.props.cart.movies) {
+      return (
+        <div className="cart">
+          <p>Items In Your Cart:</p>
+          {this.props.cart.movies.map(movie => (
+            <div key={movie.id}>
+              <img src={movie.image} />
+              <p>{movie.name}</p>
+              <p>${movie.price / 100}</p>
+              <button onClick={() => this.props.removeMovie(movie)}>
+                Remove from Cart
+              </button>
+            </div>
+          ))}
+          <p>Total Price: ${totalPrice / 100}</p>
+
+          <Link to="/postcheckout">
+            <button
+              onClick={() => {
+                this.props.checkout(this.props.user.id)
+                this.props.renderCart()
+              }}
+            >
+              Submit Order
+            </button>
+          </Link>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <p>Your cart is empty</p>
+        </div>
+      )
+    }
+  }
+}
+
+const mapState = state => {
+  return {
+    user: state.user,
+    cart: state.cart.userCart
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    renderCart: () => {
+      dispatch(getUserCartById())
+    },
+    removeMovie: movie => {
+      dispatch(removeMovieThunk(movie))
+    },
+    checkout: userId => {
+      dispatch(checkOutThunk(userId))
+    }
+  }
+}
+
+export default connect(mapState, mapDispatch)(Checkout)
