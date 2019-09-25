07bb5a1107bb6cd12d75aa07dd961202c6ac9381
Michael Cheung <mcheung125@gmail.com>
2019-09-24 20:59:13 -0400

    secrets

---
+++ secrets.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    email: 'ai@gmail.com',
+    password: '123',
+    id: 1,
+    isAdmin: true
+  },
+  {
+    email: 'ethan@gmail.com',
+    password: '345',
+    id: 2
+  },
+  {
+    email: 'jon@gmail.com',
+    password: '567',
+    id: 3
+  },
+  {
+    email: 'mike@gmail.com',
+    password: '789',
+    id: 4
+  }
+]
